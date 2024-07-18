b58cea17d408d95f5cf58b8dbe32b98919c02cc5
John Sameh <jsameh16@gmail.com>
2024-07-18 19:57:44 +0300

    Save Functionality

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    email = email_entry.get()
+    password = password_entry.get()
+    website = website_entry.get()
+
+    with open("/Users/SAMEH/Desktop/web/python/password-manager/data.txt", mode="a") as file:
+        file.write(f"{website} | {email} | {password}\n")
+
+    password_entry.delete(0, 'end')
+    website_entry.delete(0, 'end')
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -28,6 +38,7 @@ website_entry.grid(row=1, column=1, columnspan=2)
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "jsameh16@gmail.com")
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1 )
@@ -35,7 +46,7 @@ password_entry.grid(row=3, column=1 )
 generate_button = Button(text="Generate Password")
 generate_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
